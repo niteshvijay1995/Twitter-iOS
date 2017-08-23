6a53782bd1a928da6a93078a47eb8ee0dc064061
Nitesh Vijay <niteshvijay1995@gmail.com>
2017-08-23 14:05:02 +0530

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by nitesh.vi on 23/08/17.
+//  Copyright © 2017 TNET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by nitesh.vi on 23/08/17.
+//  Copyright © 2017 TNET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
