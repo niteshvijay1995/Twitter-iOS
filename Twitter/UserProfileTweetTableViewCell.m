30ef2496456a276053aed0e8ca78ec71703a284a
Nitesh Vijay <niteshvijay1995@gmail.com>
2017-08-31 12:53:54 +0530

    Refactor removed unused code

--- Twitter/UserProfileTweetTableViewCell.m
+++ Twitter/UserProfileTweetTableViewCell.m
@@ -100,51 +100,6 @@
     
 }
 
-/* For Exploration - programatically creating auto layout contraints
-- (void)addToCellTweetLabel:(NSDictionary *)tweet {
-    self.tweetTextLabel = [[UILabel alloc] init];
-    [self.tweetTextLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
-    self.tweetTextLabel.text = [tweet valueForKeyPath:TWITTER_TWEET_TEXT];
-    [self.tweetTextLabel setFont:[UIFont fontWithName:@"System" size:13]];
-    [self.tweetTextLabel sizeToFit];
-    self.tweetTextLabel.numberOfLines = 0;
-    [self.contentView addSubview:self.tweetTextLabel];
-    
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:self.tweetTextLabel attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.userFullNameLabel attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0]];
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:self.tweetTextLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.userFullNameLabel attribute:NSLayoutAttributeBottom multiplier:1.0 constant:4.0]];
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:self.tweetTextLabel attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeBottomMargin multiplier:1.0 constant:4.0]];
-}
-
-- (void)addToCellFullNameLabel:(NSDictionary *)user {
-    self.userFullNameLabel = [[UILabel alloc] init];
-    [self.userFullNameLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
-    self.userFullNameLabel.text = [user valueForKeyPath:TWITTER_USER_FULL_NAME];
-    [self.userFullNameLabel setFont:[UIFont fontWithName:@"System Bold" size:14]];
-    [self.userFullNameLabel sizeToFit];
-    [self.contentView addSubview:self.userFullNameLabel];
-    
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:self.userFullNameLabel attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.userProfileImageView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:5.0]];
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:self.userFullNameLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeTopMargin multiplier:1.0 constant:5.0]];
-    
-}
-
-- (void)addToCellProfileImageOfUser:(NSDictionary *)user {
-    //TO-DO Set Image size according to screen size
-    self.userProfileImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
-    [self.userProfileImageView setTranslatesAutoresizingMaskIntoConstraints:NO];
-    self.userProfileImageView.image = [UIImage imageNamed:@"default_profile_normal"];
-    self.userProfileImageView.layer.cornerRadius = self.userProfileImageView.frame.size.height/2;
-    self.userProfileImageView.clipsToBounds = YES;
-    [self.contentView addSubview:self.userProfileImageView];
-    
-    [self.userProfileImageView addConstraint:[NSLayoutConstraint constraintWithItem:self.userProfileImageView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:50]];
-    [self.userProfileImageView addConstraint:[NSLayoutConstraint constraintWithItem:self.userProfileImageView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:50]];
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:self.userProfileImageView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeTopMargin multiplier:1.0 constant:2.0]];
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:self.userProfileImageView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeLeftMargin multiplier:1.0 constant:2.0]];
-    
-}
-*/
-
 - (void)addToCellTime:(NSString *)time {
     NSDateFormatter * dateFormatter = [[NSDateFormatter alloc]init];
     [dateFormatter setDateFormat:@"EEEE MMM dd HH:mm:ss Z yyyy"];
