5b832c242ea09b93b99c569f0517cc20071a305a
Nitesh Vijay <niteshvijay1995@gmail.com>
2017-09-01 16:34:08 +0530

    Add animation for like button

--- Twitter/TweetOptionsFooterBarView.m
+++ Twitter/TweetOptionsFooterBarView.m
@@ -18,22 +18,48 @@
     // Drawing code
 }
 */
+
+// Animation properties
+
+static float animationTime = 0.1;
+static float likeIconAnimationSmallSizePercent = 0.75;
+static float HALF_PERCENT = 0.5;
+
+
 - (IBAction)likeButtonPressed:(UIButton *)sender {
     if (sender.tag == 0) {
         [TwitterTweet likeTweetWithId:sender.restorationIdentifier];
         sender.tag = 1;
-        self.likeIconImageView.image = [UIImage imageNamed:@"liked_icon"];
-        self.likeCountLabel.text = [NSString stringWithFormat:@"%lld",[self.likeCountLabel.text longLongValue] + 1];
-        self.likeCountLabel.textColor = [UIColor redColor];
-
-        
+        __block CGPoint origin = self.likeIconImageView.frame.origin;
+        __block CGFloat length = self.likeIconImageView.frame.size.height;
+        [UIView animateWithDuration:animationTime animations:^{
+            CGFloat newOrigin = (1-likeIconAnimationSmallSizePercent)*length*HALF_PERCENT;
+            self.likeIconImageView.frame = CGRectMake(newOrigin, newOrigin, likeIconAnimationSmallSizePercent*length, likeIconAnimationSmallSizePercent*length) ;
+        } completion:^(BOOL finished) {
+            self.likeIconImageView.image = [UIImage imageNamed:@"liked_icon"];
+            self.likeCountLabel.text = [NSString stringWithFormat:@"%lld",[self.likeCountLabel.text longLongValue] + 1];
+            self.likeCountLabel.textColor = [UIColor redColor];
+            [UIView animateWithDuration:animationTime animations:^{
+                self.likeIconImageView.frame = CGRectMake(origin.x, origin.y, length, length);
+            }];
+        }];
     }
     else {
         [TwitterTweet unlikeTweetWithId:sender.restorationIdentifier];
         sender.tag = 0;
-        self.likeIconImageView.image = [UIImage imageNamed:@"like_icon"];
-        self.likeCountLabel.text = [NSString stringWithFormat:@"%lld",[self.likeCountLabel.text longLongValue] - 1];
-        self.likeCountLabel.textColor = [UIColor blackColor];
+        __block CGPoint origin = self.likeIconImageView.frame.origin;
+        __block CGFloat length = self.likeIconImageView.frame.size.height;
+        [UIView animateWithDuration:animationTime animations:^{
+            CGFloat newOrigin = (1-likeIconAnimationSmallSizePercent)*length*HALF_PERCENT;
+            self.likeIconImageView.frame = CGRectMake(newOrigin, newOrigin, likeIconAnimationSmallSizePercent*length, likeIconAnimationSmallSizePercent*length) ;
+        }completion:^(BOOL finished) {
+            self.likeIconImageView.image = [UIImage imageNamed:@"like_icon"];
+            self.likeCountLabel.text = [NSString stringWithFormat:@"%lld",[self.likeCountLabel.text longLongValue] - 1];
+            self.likeCountLabel.textColor = [UIColor blackColor];
+            [UIView animateWithDuration:animationTime animations:^{
+                self.likeIconImageView.frame = CGRectMake(origin.x, origin.y, length, length);
+            }];
+        }];
     }
 }
 - (IBAction)retweetButtonPressed:(UIButton *)sender {
