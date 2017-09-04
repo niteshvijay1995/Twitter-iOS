815b56afa69ddb7f5dcff19aa7d9684bb554c39f
Nitesh Vijay <niteshvijay1995@gmail.com>
2017-09-04 15:36:30 +0530

    Fix refreshHomeScreen to fetch new tweets diff only

--- Twitter/HomeScreenTweetFetcherCVC.m
+++ Twitter/HomeScreenTweetFetcherCVC.m
@@ -9,6 +9,7 @@
 #import "HomeScreenTweetFetcherCVC.h"
 #import <TwitterKit/TwitterKit.h>
 #import "TwitterFetcher.h"
+#import "TwitterTweet.h"
 
 @interface HomeScreenTweetFetcherCVC ()
 
@@ -31,7 +32,50 @@ static NSString *homeTimelineEndPoint = @"https://api.twitter.com/1.1/statuses/h
 }
 
 - (void)refreshHomeScreen {
-    [self fetchTweets];
+    if (self.tweetList == nil || [self.tweetList count] == 0) {
+        [self fetchTweets];
+    }
+    else {
+        [self fetchNewTweets];
+    }
+}
+
+- (void)fetchNewTweets {
+    TWTRSessionStore *store = [[Twitter sharedInstance] sessionStore];
+    NSString *userID = store.session.userID;
+    
+    TWTRAPIClient *client = [[TWTRAPIClient alloc] initWithUserID:userID];
+    
+    NSString *since_id = [TwitterTweet getTweetIDForTweet:[self.tweetList firstObject]];
+    NSDictionary *params = @{@"count":maxTweetCountToFetch, @"since_id":since_id};
+    
+    NSError *clientError;
+    
+    NSURLRequest *request = [client URLRequestWithMethod:@"GET" URL:homeTimelineEndPoint parameters:params error:&clientError];
+    
+    if (request) {
+        dispatch_queue_t fetchHomeScreenTweetQ = dispatch_queue_create("Home Screen Tweet Fetcher", NULL);
+        dispatch_async(fetchHomeScreenTweetQ, ^{
+            fetchStatus = YES;
+            [client sendTwitterRequest:request completion:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                if (data) {
+                    NSError *jsonError;
+                    NSMutableArray *newTweetList = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                    self.tweetList = [[newTweetList arrayByAddingObjectsFromArray:self.tweetList] mutableCopy];
+                    [self.refreshControl endRefreshing];
+                    [self.collectionView reloadData];
+                    [self.collectionViewLayout invalidateLayout];
+                }
+                else {
+                    NSLog(@"Error: %@", connectionError);
+                }
+                fetchStatus = NO;
+            }];
+        });
+    }
+    else {
+        NSLog(@"Error: %@", clientError);
+    }
 }
 
 - (void)fetchTweets {
