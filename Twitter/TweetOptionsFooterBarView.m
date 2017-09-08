6e59f32cedaa3fc0d17ec0f88fb00233c6b8788c
Nitesh Vijay <niteshvijay1995@gmail.com>
2017-09-08 19:22:44 +0530

    Add Core Data integration for like and retweet button with disturbed animation

--- Twitter/TweetOptionsFooterBarView.m
+++ Twitter/TweetOptionsFooterBarView.m
@@ -8,6 +8,8 @@
 
 #import "TweetOptionsFooterBarView.h"
 #import "TwitterTweet.h"
+#import "CoreDataController.h"
+#import "Tweet+CoreDataProperties.h"
 
 @implementation TweetOptionsFooterBarView
 
@@ -25,11 +27,29 @@ static float animationTime = 0.1;
 static float likeIconAnimationSmallSizePercent = 0.75;
 static float HALF_PERCENT = 0.5;
 
+- (Tweet *)getTweetOfId:(NSString *)id fromContext:(NSManagedObjectContext *)context {
+    Tweet *tweet = nil;
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Tweet"];
+    request.predicate = [NSPredicate predicateWithFormat:@"id = %@",id];
+    
+    NSError *error;
+    NSArray *matches = [context executeFetchRequest:request error:&error];
+    if (!matches || error || [matches count] > 1) {
+        NSLog(@"More than one tweet exists with id = %@",id);
+    } else if ([matches count]) {
+        tweet = [matches firstObject];
+    }
+    return tweet;
+}
 
 - (IBAction)likeButtonPressed:(UIButton *)sender {
+    NSManagedObjectContext *context = [CoreDataController sharedInstance].managedObjectContext;
+    Tweet *tweet = [self getTweetOfId:sender.restorationIdentifier fromContext:context];
     if (sender.tag == 0) {
         [TwitterTweet likeTweetWithId:sender.restorationIdentifier];
         sender.tag = 1;
+        tweet.favorited = YES;
+        tweet.favoriteCount += 1;
         __block CGPoint origin = self.likeIconImageView.frame.origin;
         __block CGFloat length = self.likeIconImageView.frame.size.height;
         [UIView animateWithDuration:animationTime animations:^{
@@ -47,6 +67,8 @@ static float HALF_PERCENT = 0.5;
     else {
         [TwitterTweet unlikeTweetWithId:sender.restorationIdentifier];
         sender.tag = 0;
+        tweet.favorited = NO;
+        tweet.favoriteCount -= 1;
         __block CGPoint origin = self.likeIconImageView.frame.origin;
         __block CGFloat length = self.likeIconImageView.frame.size.height;
         [UIView animateWithDuration:animationTime animations:^{
@@ -61,11 +83,16 @@ static float HALF_PERCENT = 0.5;
             }];
         }];
     }
+    [context save:NULL];
 }
 - (IBAction)retweetButtonPressed:(UIButton *)sender {
+    NSManagedObjectContext *context = [CoreDataController sharedInstance].managedObjectContext;
+    Tweet *tweet = [self getTweetOfId:sender.restorationIdentifier fromContext:context];
     if (sender.tag == 0) {
         [TwitterTweet retweetTweetWithId:sender.restorationIdentifier];
         sender.tag = 1;
+        tweet.retweeted = YES;
+        tweet.retweetCount += 1;
         [UIView animateWithDuration:0.2f animations:^{
             self.retweetIconImageView.transform = CGAffineTransformMakeRotation((180.0 * M_PI) / 180.0);
         }completion:^(BOOL finished) {
@@ -77,6 +104,8 @@ static float HALF_PERCENT = 0.5;
     else {
         [TwitterTweet unretweetTweetWithId:sender.restorationIdentifier];
         sender.tag = 0;
+        tweet.retweeted = NO;
+        tweet.retweetCount -= 1;
         [UIView animateWithDuration:0.2f animations:^{
             self.retweetIconImageView.transform = CGAffineTransformMakeRotation((180.0 * M_PI) * 180.0);
         }completion:^(BOOL finished) {
@@ -85,6 +114,7 @@ static float HALF_PERCENT = 0.5;
             self.retweetCountLabel.textColor = [UIColor blackColor];
         }];
     }
+    [context save:NULL];
 }
 
 @end
