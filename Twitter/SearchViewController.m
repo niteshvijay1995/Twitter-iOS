2891b271d2451bfde2386fc74143925ce1018a7d
Nitesh Vijay <niteshvijay1995@gmail.com>
2017-09-14 13:28:07 +0530

    Add container view in SearchViewController to embed CoreDataTweetsCollectionViewController for searchResultsView

--- Twitter/SearchViewController.m
+++ Twitter/SearchViewController.m
@@ -10,19 +10,18 @@
 #import "HomeScreenCDTCVC.h"
 #import "CoreDataController.h"
 #import "TweetCollectionViewCell.h"
+#import "CoreDataTweetsCollectionViewController.h"
 
-@interface SearchViewController() <UISearchBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource, NSFetchedResultsControllerDelegate>
+@interface SearchViewController() <UISearchBarDelegate>
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
-@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (nonatomic, strong) NSManagedObjectContext *tweeteManagedObjectContext;
-@property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
 @property (nonatomic, strong) NSFetchRequest *request;
-@property (strong, nonatomic) NSBlockOperation *blockOperation;
-@property BOOL shouldReloadCollectionView;
-@property BOOL debug;
+@property (nonatomic, strong) CoreDataTweetsCollectionViewController *searchResultsCVC;
 @end
 
 @implementation SearchViewController
+
+static NSString * const reuseIdentifier = @"TweetCell";
 
 - (NSManagedObjectContext *)tweeteManagedObjectContext {
     if (!_tweeteManagedObjectContext) {
@@ -42,10 +41,8 @@
 -(void)viewDidLoad {
     [super viewDidLoad];
     self.searchBar.delegate = self;
-    self.collectionView.delegate = self;
-    self.collectionView.dataSource = self;
-    [self.collectionView registerNib:[UINib nibWithNibName:@"TweetCell" bundle:NULL] forCellWithReuseIdentifier:reuseIdentifier];
-    ((UICollectionViewFlowLayout *)self.collectionView.collectionViewLayout).estimatedItemSize = CGSizeMake(1, 1);
+    self.searchResultsCVC = [[self childViewControllers] firstObject];
+    [self.searchResultsCVC disableRefreshControl];
 }
 
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
@@ -56,180 +53,13 @@
     if ([searchText isEqualToString:@""]) {
         self.request.predicate = [NSCompoundPredicate orPredicateWithSubpredicates:@[namePredicate]];
     }
-    self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest: self.request managedObjectContext:self.tweeteManagedObjectContext sectionNameKeyPath:nil cacheName:nil];
+    self.searchResultsCVC.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest: self.request managedObjectContext:self.tweeteManagedObjectContext sectionNameKeyPath:nil cacheName:nil];
 }
 
-#pragma mark - CoreData
-
-- (void)performFetch {
-    if (self.fetchedResultsController) {
-        if (self.fetchedResultsController.fetchRequest.predicate) {
-            if (self.debug) NSLog(@"[%@ %@] fetching %@ with predicate: %@", NSStringFromClass([self class]),
-                                  NSStringFromSelector(_cmd), self.fetchedResultsController.fetchRequest.entityName, self.fetchedResultsController.fetchRequest.predicate);
-        } else {
-            if (self.debug) NSLog(@"[%@ %@] fetching all %@ (i.e., no predicate)", NSStringFromClass([self class]), NSStringFromSelector(_cmd), self.fetchedResultsController.fetchRequest.entityName);
-        }
-        NSError *error;
-        BOOL success = [self.fetchedResultsController performFetch:&error];
-        if (!success) NSLog(@"[%@ %@] performFetch: failed", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
-        if (error) NSLog(@"%@ %@] %@ (%@)", NSStringFromClass([self class]), NSStringFromSelector(_cmd), [error localizedDescription], [error localizedFailureReason]);
-        NSLog(@"fetchedResultsController found %d objects", (int)[self.fetchedResultsController.fetchedObjects count]);
-    } else {
-        if (self.debug) NSLog(@"[%@ %@] no NSFetchedResultsController (yet?)", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
-    }
-    [self.collectionView reloadData];
-    [self.collectionView.collectionViewLayout invalidateLayout];
-}
-
-- (void)setFetchedResultsController:(NSFetchedResultsController *)newfrc {
-    NSFetchedResultsController *oldfrc = _fetchedResultsController;
-    if (newfrc != oldfrc) {
-        _fetchedResultsController = newfrc;
-        newfrc.delegate = self;
-        if ((!self.title || [self.title isEqualToString:oldfrc.fetchRequest.entity.name]) && (!self.navigationController || !self.navigationItem.title)) {
-            self.title = newfrc.fetchRequest.entity.name;
-        }
-        if (newfrc) {
-            if (self.debug) NSLog(@"[%@ %@] %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), oldfrc ? @"updated" : @"set");
-            [self performFetch];
-        } else {
-            if (self.debug) NSLog(@"[%@ %@] reset to nil", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
-            [self.collectionView reloadData];
-        }
-    }
-}
-
-#pragma mark <UICollectionViewDataSource>
-
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    NSInteger sections = [[self.fetchedResultsController sections] count];
-    return sections;
-}
-
-
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    NSInteger items = 0;
-    if ([[self.fetchedResultsController sections] count] > 0) {
-        id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
-        items = [sectionInfo numberOfObjects];
-    }
-    return items;
-}
-
-static NSString * const reuseIdentifier = @"TweetCell";
-
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    TweetCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
-    
-    NSLog(@"Configuring cell at indexPath - %@",indexPath);
-    Tweet *tweet = [self.fetchedResultsController objectAtIndexPath:indexPath];
-    
-    [cell configureCellFromCoreDataTweet:tweet];
-    NSLog(@"Configuration Done");
-    return cell;
-}
-
-
-#pragma mark - <NSFetchedResultsControllerDelegate>
-
-- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
-    self.shouldReloadCollectionView = NO;
-    self.blockOperation = [[NSBlockOperation alloc] init];
-}
-
-- (void)controller:(NSFetchedResultsController *)controller
-  didChangeSection:(nonnull id<NSFetchedResultsSectionInfo>)sectionInfo
-           atIndex:(NSUInteger)sectionIndex
-     forChangeType:(NSFetchedResultsChangeType)type
-{
-    __weak UICollectionView *collectionView = self.collectionView;
-    switch (type) {
-        case NSFetchedResultsChangeInsert: {
-            [self.blockOperation addExecutionBlock:^{
-                [collectionView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex]];
-            }];
-            break;
-        }
-        case NSFetchedResultsChangeDelete: {
-            [self.blockOperation addExecutionBlock:^{
-                [collectionView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex]];
-            }];
-            break;
-        }
-        case NSFetchedResultsChangeUpdate: {
-            [self.blockOperation addExecutionBlock:^{
-                [collectionView reloadSections:[NSIndexSet indexSetWithIndex:sectionIndex]];
-            }];
-            break;
-        }
-            
-        default:
-            break;
-    }
-}
-
-- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath
-{
-    __weak UICollectionView *collectionView = self.collectionView;
-    switch (type) {
-        case NSFetchedResultsChangeInsert: {
-            if ([self.collectionView numberOfSections] > 0) {
-                if ([self.collectionView numberOfItemsInSection:indexPath.section] == 0) {
-                    self.shouldReloadCollectionView = YES;
-                } else {
-                    [self.blockOperation addExecutionBlock:^{
-                        [collectionView insertItemsAtIndexPaths:@[newIndexPath]];
-                    }];
-                }
-            } else {
-                self.shouldReloadCollectionView = YES;
-            }
-            break;
-        }
-            
-        case NSFetchedResultsChangeDelete: {
-            if ([self.collectionView numberOfItemsInSection:indexPath.section] == 1) {
-                self.shouldReloadCollectionView = YES;
-            } else {
-                [self.blockOperation addExecutionBlock:^{
-                    [collectionView deleteItemsAtIndexPaths:@[indexPath]];
-                }];
-            }
-            break;
-        }
-            
-        case NSFetchedResultsChangeUpdate: {
-            [self.blockOperation addExecutionBlock:^{
-                [collectionView reloadItemsAtIndexPaths:@[indexPath]];
-            }];
-            break;
-        }
-            
-        case NSFetchedResultsChangeMove: {
-            [self.blockOperation addExecutionBlock:^{
-                [collectionView moveItemAtIndexPath:indexPath toIndexPath:newIndexPath];
-            }];
-            break;
-        }
-            
-        default:
-            break;
-    }
-}
 - (IBAction)tapToDisableKeyboard:(id)sender {
     [self.view endEditing:YES];
 }
 
-- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller
-{
-    if (self.shouldReloadCollectionView) {
-        [self.collectionView reloadData];
-    } else {
-        [self.collectionView performBatchUpdates:^{
-            [self.blockOperation start];
-        } completion:nil];
-    }
-}
 
 
 
