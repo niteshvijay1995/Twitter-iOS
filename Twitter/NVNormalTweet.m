ec959b638849e8cd04a4da408db67bd0a7f4f850
Nitesh Vijay <niteshvijay1995@gmail.com>
2017-09-15 12:15:09 +0530

    Ad NVNormalTweet following NVTweet Protocol

---
+++ Twitter/NVNormalTweet.m
@@ -0,0 +1,13 @@
+//
+//  NVNormalTweet.m
+//  Twitter
+//
+//  Created by nitesh.vi on 15/09/17.
+//  Copyright © 2017 TNET. All rights reserved.
+//
+
+#import "NVNormalTweet.h"
+
+@implementation NVNormalTweet
+
+@end
